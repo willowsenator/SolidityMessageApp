3310bb2768ad97272d222a47527706d38b450692
willowsenator <willowsenator@gmail.com>
2023-05-14 13:38:10 +0200

    Add MessagingCounter and MessagingManager smart contracts

---
+++ contracts/MessagingCounter.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.0 < 0.9.0;
+
+import "./MessagingManager.sol";
+
+contract MessagingCounter {
+    MessagingManager messaging_manager;
+
+    constructor (address _address_messaging_manager){
+        messaging_manager = MessagingManager(_address_messaging_manager);
+    }
+
+    function getNumMessages() public view returns(uint){
+        return messaging_manager.getAllMessages().length;
+    }
+}
